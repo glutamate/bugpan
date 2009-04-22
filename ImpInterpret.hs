b71fc2be7a91b22707e0fa26eaa3257c3d8fb509
Tom Nielsen <tanielsen@gmail.com>
2009-04-22 23:00:05 +0200

    bit of work on interpreter

--- ImpInterpret.hs
+++ ImpInterpret.hs
@@ -5,19 +5,30 @@ import EvalM
 import Eval
 import Compiler
 import Numbers
+import Control.Monad.State.Strict
+import Data.IORef
+
+data InterpState = IS { sigs :: [(String, V)],
+                        evts :: [(String, [(Double,V)])] }
 
 exec :: [Stmt] -> Double -> Double -> IO ()
 exec stmts dt tmax = 
-    let initsigs = [(nm,v) | InitSig nm v <-  stmts]
-        prg =  filter isSigOrEvtS stmts 
+    let prg =  filter isSigOrEvtS stmts 
         fixEnvEs = ("dt",Const . NumV . NReal$ dt):[(nm,v) | en@(Env nm v) <-  stmts]
         emptyEvalS = EvalS 1 1 Nothing []
         fixEnv = map (\(n,e)->(n,unEvalM $ eval fixEvalS e)) fixEnvEs
         fixEvalS =  extsEnv fixEnv emptyEvalS
+        initSigs = [(nm, unEvalM $ eval fixEvalS e) | InitSig nm e <-  stmts]
+        initEvts = [ (nm,[]) | EventUpdateRule nm _ <- stmts]
         nsteps :: Int
         nsteps = round $ tmax/dt
-        ts = map ((*dt) . realToFrac) [0..nsteps]
-    in undefined
+        ts = map ((*dt) . realToFrac) [0..nsteps] in
+    do sigs <- newIORef initSigs
+       evts <- newIORef initEvts
+       forM ts $ \t-> 
+            do forM prg $ \stm -> do
+                 case stm of 
+                   SigUpdateRule nm e -> 
 
 updateEvent :: EvalS -> E -> [V]
 updateEvent es e = case unEvalM $ eval es e of 
