16c11e6e69d8876fd4d3b5e31c34d05aeca83e21
Tom Nielsen <tomn@myhost.(none)>
2009-05-10 19:29:27 +0100

    giving up on typed hasksyntax. last before change

--- HaskSyntax.hs
+++ HaskSyntax.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, FunctionalDependencies, UndecidableInstances, FlexibleContexts, GeneralizedNewtypeDeriving, NoMonomorphismRestriction #-}
 
 module HaskSyntax where
 
 import qualified Expr as U
 import EvalM
 import Data.List
+import Numbers
+
+default (Int, Double)
 
 infixl 1 =:
 
@@ -14,8 +17,9 @@ class Assignable s where
 instance Assignable U.Declare where
     x =: y = let (nm:vars) = splitBySpaces x in 
              U.Let nm $ addLams (reverse vars) y 
-        where addLams [] e = e
-              addLams (v:vs) e = addLams vs $ U.Lam v e
+
+addLams [] e = e
+addLams (v:vs) e = addLams vs $ U.Lam v e
 
 instance Assignable (String, U.E) where -- LetE
     x =: y = (x, y)
@@ -30,7 +34,7 @@ splitBySpaces' s = let (hd, tl) = partition (/=' ') s
                    in hd : splitBySpaces tl
 
 
-class ToE a b where
+class ToE a b | b->a where
     toE :: a-> TE b
 
 instance ToE [Char] a where
@@ -39,20 +43,39 @@ instance ToE [Char] a where
 instance ToE (TE a) a where
     toE = id
 
+instance ToE Int Int where
+    toE i = TE . U.Const . NumV . NInt $ i 
+
+--instance ToE (TE Int) Int where
+--    toE = id
+
 infixl 1 ~>                    
 
 x ~> y = (x, y)            
 
-newtype TE a = TE { unTE :: U.E }
+newtype TE a = TE { unTE :: U.E } deriving (Eq, Show)
+
+--instance Show (TE a) where
+--    show x= show $ unTE x
+
+--instance Num a =>Num (TE a) where
+--    fromInteger i = TE $ U.Const . NumV . NInt . fromInteger $ i
 
 true, false :: TE Bool
 true = TE . U.Const . BoolV $ True
 false = TE . U.Const . BoolV $ False
 
-ifE :: TE Bool -> TE a -> TE a -> TE a
-ifE p c a = TE $ U.If (unTE p) (unTE c) (unTE a)
+iff :: (ToE p Bool, ToE c r, ToE a r) => p -> c -> a -> TE r
+iff p c a = TE $ U.If (unTE . toE $ p) (unTE. toE $  c) (unTE. toE $  a)
 
 lam :: (ToE a b) => String -> a -> TE (b->c)
-lam vn bod = TE $ U.Lam vn (unTE (toE bod))
+lam vn bod = --let vars = splitBySpaces vn in  .. addLams (reverse vars) 
+             TE $ U.Lam vn (unTE (toE bod))
 
--- ($>) :: TE (a->b) -> TE a -> TE B
+($>) :: (ToE sab (a->b), ToE sa a) => sab -> sa -> TE b
+lm $> ag = TE $ U.App (unTE . toE $ lm) (unTE . toE $ ag)
+
+foo = iff true "x" "y"
+
+bar :: TE Int
+bar = (lam "x" "x") $> (1::Int)
