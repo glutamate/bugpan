a8f7a7c8e76cb00e82b4c1a357a29d9d5eee7922
Tom Nielsen <tomn@bl-0057.cfs.le.ac.uk>
2009-05-15 12:09:26 +0100

    ask monad in database

--- Database.hs
+++ Database.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-} 
+
 module Database where
 
 import EvalM
@@ -12,7 +14,8 @@ import Stages
 import Traverse
 import Transform
 import Control.Monad
-
+import Control.Monad.List
+import Control.Monad.State.Lazy
 
 data Session = Session { events :: [(String, [(Double,V)])],
                          sigSegments :: [(String, [(Double, Double, Double->V)])],
@@ -107,56 +110,7 @@ prevTrialStart sess = case programsRun sess of
                         ((t1,t2,_):_) -> t1
   --return sess
 
-data Q = QVar String
-       -- | Filter E Q
-       -- | Map E Q
-       | ApplyToAll E Q
-       | Filter E Q
-       | Map E Q
-       | Has Q Q
-       | In Q Q
-       | Around Q Q
-       | Bind String Q Q
 
-ask :: Session -> Q -> EvalM [V]
-ask sess (QVar nm) 
-    = case lookup nm $ qenv sess of
-        Just (ListV vs) -> return vs
-        Just v -> return [v]
-        Nothing -> lookupInSigs
-    where lookupInSigs =  case lookup nm $ sigSegments sess of
-                            Just vls -> return $ map (\(t1,t2,sf)-> SigV t1 t2 sf) vls
-                            Nothing -> lookupInEvts
-          lookupInEvts = case lookup nm $ events sess of
-                            Just vls -> return $ map (\(t,v)-> PairV (NumV . NReal $ t) (v)) vls
-                            Nothing -> return []
-ask sess (ApplyToAll lame q) = do vs <- ask sess q
-                                  mkList `fmap` eval (sessEvalState sess) (App lame (Const $ ListV vs))
-ask sess (Map lame q) = do 
-  vs <- ask sess q
-  forM vs $ \v-> eval (sessEvalState sess) (App lame (Const v))
-ask sess (Bind nm q qrest) = do vs <- ask sess q
-                                ask (sess {qenv = (nm,ListV vs): qenv sess}) qrest
-ask sess (Around qsig qevt) = do 
-  sigs <- ask sess qsig
-  let sigDefnInTm t (SigV t1 t2 _) = t<t2 && t>t1
-  let inRanges t = any (sigDefnInTm t) sigs
-  evts <-  filter (inRanges . evTime) `fmap` ask sess qevt
-  return $ map (\e-> let sig = head . filter (sigDefnInTm $ evTime e) $ sigs
-                     in shiftSig sig $ evTime e) evts
-
-ask sess (Has qep qevs) = do 
-  evs <- ask sess qevs
-  filter (\ep-> any (`evInEpoch` ep) evs) `fmap` ask sess qep
-ask sess (In qevOrSig qep) = do 
-  eps <- ask sess qep
-  evOrSig <- ask sess qevOrSig
-  let ress = for evOrSig $ \eos-> case eos of
-                                     s@(SigV t1 t2 sf) -> sigInEps s eps 
-                                     ev@(PairV (NumV (NReal t1)) v) -> if any (evInEpoch ev) eps
-                                                                         then [ev]
-                                                                         else []
-  return $ concat ress
 
 for = flip map
 
@@ -196,3 +150,64 @@ mkList v = [v]
 evInEpoch ev ep = let (t1, t2) = epTs ep 
                       tev = evTime ev
                   in tev<t2 && tev>t1
+
+
+newtype AskM a = AskM { unAskM :: ListT (StateT Session IO) a }
+    deriving (Monad, MonadIO, Functor, MonadState Session)
+
+runAskM :: Session -> AskM a -> IO [a]
+runAskM sess (AskM lsioA) = fst `fmap` runStateT (runListT (lsioA)) sess
+
+--askM :: Q -> AskM V
+--askM (QVar "nm") = do 
+
+data Q = QVar String
+       -- | Filter E Q
+       -- | Map E Q
+       | ApplyToAll E Q
+       | Filter E Q
+       | Map E Q
+       | Has Q Q
+       | In Q Q
+       | Around Q Q
+       | Bind String Q Q
+
+--ask :: Q -> AskM V
+ask sess (QVar nm) 
+    = case lookup nm $ qenv sess of
+        Just (ListV vs) -> return vs
+        Just v -> return [v]
+        Nothing -> lookupInSigs
+    where lookupInSigs =  case lookup nm $ sigSegments sess of
+                            Just vls -> return $ map (\(t1,t2,sf)-> SigV t1 t2 sf) vls
+                            Nothing -> lookupInEvts
+          lookupInEvts = case lookup nm $ events sess of
+                            Just vls -> return $ map (\(t,v)-> PairV (NumV . NReal $ t) (v)) vls
+                            Nothing -> return []
+ask sess (ApplyToAll lame q) = do vs <- ask sess q
+                                  mkList `fmap` eval (sessEvalState sess) (App lame (Const $ ListV vs))
+ask sess (Map lame q) = do 
+  vs <- ask sess q
+  forM vs $ \v-> eval (sessEvalState sess) (App lame (Const v))
+ask sess (Bind nm q qrest) = do vs <- ask sess q
+                                ask (sess {qenv = (nm,ListV vs): qenv sess}) qrest
+ask sess (Around qsig qevt) = do 
+  sigs <- ask sess qsig
+  let sigDefnInTm t (SigV t1 t2 _) = t<t2 && t>t1
+  let inRanges t = any (sigDefnInTm t) sigs
+  evts <-  filter (inRanges . evTime) `fmap` ask sess qevt
+  return $ map (\e-> let sig = head . filter (sigDefnInTm $ evTime e) $ sigs
+                     in shiftSig sig $ evTime e) evts
+
+ask sess (Has qep qevs) = do 
+  evs <- ask sess qevs
+  filter (\ep-> any (`evInEpoch` ep) evs) `fmap` ask sess qep
+ask sess (In qevOrSig qep) = do 
+  eps <- ask sess qep
+  evOrSig <- ask sess qevOrSig
+  let ress = for evOrSig $ \eos-> case eos of
+                                     s@(SigV t1 t2 sf) -> sigInEps s eps 
+                                     ev@(PairV (NumV (NReal t1)) v) -> if any (evInEpoch ev) eps
+                                                                         then [ev]
+                                                                         else []
+  return $ concat ress
