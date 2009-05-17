6ca3a10c98ab2bc199ad6fa7237fa7014c3b1bcb
Tom Nielsen <tomn@myhost.(none)>
2009-05-17 21:44:05 +0100

    bit more daq

--- Daq.hs
+++ Daq.hs
@@ -1,4 +1,7 @@
 module Daq where
+import Foreign.C
+import Foreign.Ptr
+import Foreign.Marshal.Array
 
 import Comedi.Comedi
 
@@ -17,4 +20,22 @@ setupInputChannel ch tmax = do
   let delt = (1.0/(realToFrac globFreq)) -- $ acqRateHz ch'))
   let npts = round $ tmax/delt
   let dn = round (globFreq / realToFrac (acqRateHz ch)) -1
-  setupReadWave ch npts
+  fromIntegral `fmap` setupReadWave ch npts
+
+retrieveInputWave :: Int -> Int -> IO [Double]
+retrieveInputWave nprom npnts = do
+  ptr <- get_wave_ptr (fromIntegral nprom)
+  fmap realToFrac $ peekArray npnts ptr
+
+{-prom2res (WvProm nm nchan npnts delt dn) 
+    = do ptr <- get_wave_ptr (fromInteger nchan)
+         lst <- peekArray npnts ptr
+	 let newUVec = toU $ map toDouble lst
+         --putStr $ "decimate by "++show dn
+         return (nm, AnyRes {-. decimate dn -}$ UVecWave (newUVec) 
+                                                        delt 
+                                                        0.0
+                                                        (lengthU newUVec))
+         where toDouble :: Real a => a -> Double
+               toDouble = realToFrac
+-}
