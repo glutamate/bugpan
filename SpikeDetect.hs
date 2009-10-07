80fb43021311c4cc3424477d121846e091a1f7a7
Tom Nielsen <tomn@tomn-desktop.(none)>
2009-10-07 20:58:24 +0100

    averaging sigs in clusters

--- SpikeDetect.hs
+++ SpikeDetect.hs
@@ -68,6 +68,9 @@ vecSubMean vec = let lst = toList vec
 
 listToPoint (x:y:_) = (x,y)
 
+indexMany :: [a] -> [Int] -> [a]
+indexMany = map . (!!) 
+
 autoSpikes sigNm = do
   sigs <- take 10 `fmap` signalsDirect sigNm 
   let sd = stdDevF `sigStat` sigs
@@ -75,9 +78,11 @@ autoSpikes sigNm = do
   let putatives = crossesDown (((*5) . negate) <$$> sd) sigs `catevents` 
                   crossesUp ((*5) <$$> sd) sigs
   let waveforms = limitSigs' (-0.001) 0.001 $ around putatives $ sigs
-  let alignWaveforms = downsample 10 $ unjitter $ upsample 10 $ alignBy (centreOfMass . (square <$$>) . (limitSigs (-0.0005) 0.0005)) waveforms 
+  let alignWaveforms = take 1000 $ downsample 10 $ unjitter $ upsample 10 $ 
+                       alignBy (centreOfMass . (square <$$>) . (limitSigs (-0.0005) 0.0005)) $ 
+                       waveforms 
   --PCA
-  let dataMatrix = fromLists $ map sigToList $ take 1000 $ alignWaveforms
+  let dataMatrix = fromLists $ map sigToList $ alignWaveforms
   let datMatSubMeans = fromColumns $ map vecSubMean $ toColumns dataMatrix
   let covDM = cov datMatSubMeans
   let (eigvals, eigvecs) = eig covDM
@@ -85,9 +90,14 @@ autoSpikes sigNm = do
   let featureVector = fromColumns $ map (mapVector realPart . ((toColumns eigvecs)!!)) evs
   let finalData = trans featureVector <> trans datMatSubMeans
   let pts =  map toList $ toColumns $ finalData
-  let clustered = map (map (listToPoint . tail)) $ kmeansOn (tail) 4 $  zipWith (:) [0..] pts
-  ask $ plot $ map listToPoint pts
-  ask $ plot $ clustered!!0 :+: clustered!!1 :+: clustered!!2 :+: clustered!!3
+  let clustered = kmeansOn (snd) 3 $  zip [0..] pts
+  let clusteredvs = map (map (listToPoint . snd)) clustered
+  let idxs = map (map fst) clustered
+  let sigsav = map (head . averageSigs . (alignWaveforms `indexMany`) . map round) idxs 
+  io $ print $ length sigsav
+  ask $ plot $ sigsav
+  --ask $ plot $ map listToPoint pts
+  ask $ plot $ clusteredvs!!0 :+: clusteredvs!!1 :+: clusteredvs!!2 -- :+: clusteredvs!!3
   ask $ plot $ take 1000 $ alignWaveforms
   --io $ print $ (rows finalData, cols finalData)
 
