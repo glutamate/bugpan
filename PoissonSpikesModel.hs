07d89ed7451a0c7273dbce2caa46b10f4447bf06
Tom Nielsen <tomn@tomn-desktop.(none)>
2010-01-11 21:53:01 +0000

    likelihood model of poisson spikes

--- PoissonSpikesModel.hs
+++ PoissonSpikesModel.hs
@@ -24,8 +24,18 @@ priorSampler =
        t0 <- uniform 4 6
        return (rate, tau, baseline, t0)
 
-likelihood (rate, tau, baseline, t0) spikes =
-    undefined
+likelihood pars@(rate, tau, baseline, t0) spikes =
+    exp (integralR pars 6) * product (map (r pars . fst) spikes)
+
+only_pos x = if x>0 then x else 0
+
+r (rate, tau, baseline, t0) t 
+    = only_pos((-(t-t0)/tau)*exp(1+(t-t0)/tau))*(rate-baseline)+baseline
+--http://integrals.wolfram.com/index.jsp?expr=(-(x-z)%2Ft)*Exp[1%2B(x-z)%2Ft]*(r-b)%2Bb&random=false
+
+integralR pars@(rate, tau, baseline, t0) t 
+    | t>t0 = integralR pars t0 + baseline * t
+    | otherwise = (baseline - rate)*exp((tau+t-t0)/tau)*(-tau+t-t0)+baseline*t
 
 manyLikeOver :: ChopByDur obs => [Duration a] -> (theta -> P.PDF obs) -> (obs -> P.PDF theta)
 manyLikeOver durs lh1 = \obs-> \theta->  product $ map (lh1 theta) $ chopByDur durs obs
@@ -35,8 +45,11 @@ main =
     do inSessionFromArgs $ do          
          spike <- events "spike" ()
          running <- durations "running" ()
-         bsam <- io $ bayes 10000 (manyLikeOver running likelihood spike) priorSampler
-         ps <- take 1000 `fmap` (io $ runSamplerIO bsam)
-         io $ print $ meanSDF `runStat` (map (\(rate, tau, baseline, t0)->rate) ps)
+         bsam <- io $ bayes 100000 (manyLikeOver running likelihood spike) priorSampler
+         ps <- take 10000 `fmap` (io $ runSamplerIO bsam)
+         io $ putStrLn $ "rate "++ show ((meanSDF `both` nSumSumSqr) `runStat` map fst4 ps) 
+         io $ putStrLn $ "tau "++ show ((meanSDF `both` nSumSumSqr) `runStat` map snd4 ps)
+         io $ putStrLn $ "baseline "++ show ((meanSDF `both` nSumSumSqr) `runStat` map trd4 ps)
+         io $ putStrLn $ "t0 "++ show ((meanSDF `both` nSumSumSqr) `runStat` map fth4 ps)
 
          return ()
