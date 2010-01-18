9746fc0348ebf3264ed16512ccb94fc79a797dda
Tom Nielsen <tomn@tomn-desktop.(none)>
2010-01-18 17:02:16 +0000

    mcanalyse

---
+++ MCAnalyse.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import System.Environment
+import Data.Binary
+import System.Directory
+import Data.List
+import TNUtils
+import Data.Maybe
+import Control.Monad
+import Math.Probably.FoldingStats
+
+main = do
+  nm:restArgs <- getArgs
+  nfiles <- (length . filter (nm `isPrefixOf`)) `fmap` getDirectoryContents "."
+  let (lo, hi) = (safeHead restArgs >>= safeRead) `orJust` (0,nfiles)
+  xs <- forM [lo..hi] $ \fnum-> do 
+          let file =  (nm++show fnum++".mcmc")
+          ifM (doesFileExist file ) 
+             (Just `fmap` loadBinary file)
+             (return Nothing)
+  let bigList = (concat $ catMaybes xs) :: [[Double]]
+  putStrLn $ "#values="++show (length bigList)
+  parstr <- readFile (nm++"_parnames.mcmc") 
+  forM (zip (meanSDF `runStatOnMany` bigList) (read parstr)) $ \(estim, pnm) -> putStrLn $ (padStr 20 pnm) ++ ": "++showError estim 
+showError (mu, sd) = show mu ++ " +/- " ++show sd
+
+padStr n s | n > length s = s ++ replicate (n-length s) ' '
+           | otherwise = s
