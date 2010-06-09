94b0f782693caeeb2b59699d24b714eebb6224d4
Tom Nielsen <tomn@tomn-desktop.(none)>
2010-06-09 18:33:36 +0100

    more work on simulating a-currents

--- SimACurrent.hs
+++ SimACurrent.hs
@@ -31,21 +31,28 @@ main = do
   deleteSessionIfExists "acurrent"
   inApproxSession "new:acurrent" $ do
     step <- useFile "ACurrentStep" 
-                         (realTs "stepAmp gmaxk") []
+                         (realTs "stepAmp gmaxk rate") []
 
-    determineS step [("stepAmp", return 6.05e-10),
-                     ("gmaxk",return 1e-7)]
+    determineS step [("stepAmp", return 0),
+                     ("gmaxk",return 1e-7),
+                     ("rate",return 600)]
 
-{-    determineS step [("stepAmp", return 6.05e-10),
-                     ("gmaxk",return 0)] -}
+    determineS step [("stepAmp", return 0),
+                     ("gmaxk",return 0),
+                     ("rate",return 600)]
+
+    {-determineS step [("stepAmp", return 6.05e-10),
+                     ("gmaxk",return 0)]  -}
   inApproxSession "acurrent" $ do
          vm <- signalsDirect "vm"
          act <- signalsDirect "a"
          inact <- signalsDirect "b"
          ika <- signalsDirect "ika"
          let t0 = sigT1 $ head ika
-         let showDur = [((t0+0.99, t0+1.1), ())]
-         io $ gnuplotOnScreen $ during showDur vm :==: during showDur ika
+         let showDur = [((t0, t0+0.1), ())]
+         io $ gnuplotOnScreen $ alignBy sigStart vm 
+
+-- alignBy sigStart vm --during showDur vm :==: during showDur ika
     
 
   
