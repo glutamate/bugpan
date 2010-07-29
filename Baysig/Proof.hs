ebbd8e0e0114131f993f0af0285c4d94de0aaf3b
Tom Nielsen <tomn@tomn-eee.(none)>
2010-07-29 08:22:18 +0100

    more proof

--- Baysig/Proof.hs
+++ Baysig/Proof.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE TypeOperators #-}
 
-module Baysig.Proof (Theorem, view, (:|-:),TheoremView, refl, assume) where
+module Baysig.Proof (Theorem, v, TheoremView, refl, assume, trans) where
 
 import Baysig.Expr
+import Control.Monad
 
 data Theorem = Thm [E] E 
 
 data TheoremView = [E] :- E 
-view (Thm es e) = es :- e
+v (Thm es e) = es :- e
 
 eq x y = EVar "==" $> x $> y
+splitEq (EApp (EApp (EVar "==") x) y) = Just (x,y)
+splitEq _ = Nothing
 
-refl e = Thm [] (eq e e )
-assume e = Thm [e] e
-trans (Thm e1s e1) (Thm e2s e2) = Thm (e1s++e2s) 
+refl e = Just $ Thm [] (eq e e )
+assume e = Just $ Thm [e] e
+trans (Thm e1s e1) (Thm e2s e2) = do
+      (lx, ly) <- splitEq e1
+      (rx, ry) <- splitEq e2
+      guard (ly==rx)
+      return (Thm (e1s++e2s) (eq lx ry)) 
