a14ef52d8799055a46dacee954cdffd022f39aad
Tom Nielsen <tanielsen@gmail.com>
2011-11-29 17:22:02 +0000

    quantal analysis program done

--- FETi/Quantal.hs
+++ FETi/Quantal.hs
@@ -1,7 +1,147 @@
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables, NoMonomorphismRestriction, ViewPatterns #-}
 module Main where
 
+import System.Environment
+import Database
+import Query hiding (io) 
+import QueryTypes
+import QueryUtils hiding (averageSigs)
+import qualified QueryUtils as QU
+import Data.Maybe
+import Data.List
+import Control.Monad
+import System.Directory
+import Math.Probably.RandIO
+import QuantalHelp
+import Baysig.Estimate.RTS
+import Data.Binary
+import qualified Numeric.LinearAlgebra as L
+import Math.Probably.MCMC
+import Math.Probably.FoldingStats
+import System.IO
+import Data.Ord
+
+
+import Graphics.Gnewplot.Exec
+import Graphics.Gnewplot.Types
+import Graphics.Gnewplot.Style
+--import Graphics.Gnewplot.Panels
+import Graphics.Gnewplot.Instances
+import Graphics.Gnewplot.Histogram
+
+import Control.Monad.Trans
 
 
 
 main = do 
+  sessApprox:dowhat:_ <- getArgs
+  sess <- resolveApproxSession sessionsRootDir sessApprox
+  createDirectoryIfMissing False $ take 6 sess
+
+  when ('1' `elem` dowhat) $ epspSigs sess
+  when ('2' `elem` dowhat) $ measNoise sess
+  when ('3' `elem` dowhat) $ measAmps sess
+  when ('4' `elem` dowhat) $ measNPQ sess
+
   return ()
+
+epspSigs sess = do 
+  nms <- fmap catMaybes $ inEverySession $ whenContinues sess $ do
+     rebaseRelativeTo sess
+     vm <- signalsDirect "vm"
+     sessionIdentifier <- getSessionName
+     sessionStart <- getSessionStart
+     spike <- events "spike" ()
+     running <- durations "running" ()
+     exclude <- durations "exclude" ()
+     let swings = (\(lo,hi) -> abs(hi-lo)) <$$> sigStat (minF `both` maxF) vm
+     let noGood = contains ((>5)//swings) running
+     let spikeg = sortBy ( comparing (fst)) $ minInterval 0.1 $ notDuring exclude $ notDuring noGood spike
+     let noiseSigs = take 20 $ limitSigs' (-0.06) (-0.01) $ around (spikeg) $ vm
+     let epspSigs = during (durAroundEvent (0.01) 0.04 spikeg) vm 
+     let aroundSpike = baseline (-0.003) 0.003 $ limitSigs' (-0.05) 0.05 $ around (spikeg) $ vm
+     let ampPeak = snd $ head $ peak $ take 1 $ QU.averageSigs $ take 100 $ aroundSpike
+     let tpeak = fst $ head $ peak $ take 1 $ QU.averageSigs $ aroundSpike
+     let measDur  = measureBl (-0.003, 0.003) (tpeak-0.015,0.015+tpeak) vm spikeg
+     ask $ SaveSignals (take 6 sess++"/sigs_"++ take 6 sessionIdentifier ++ "_noise") noiseSigs
+     ask $ SaveSignals (take 6 sess++"/sigs_"++ take 6 sessionIdentifier ++ "_epsps") epspSigs
+     liftIO $ gnuplotToPNG (take 6 sess++"/epsps_"++ take 6 sessionIdentifier ++ ".png") $ measDur
+     return $ Just sessionIdentifier
+  writeFile (take 6 sess++"/sessions") $ show nms
+
+measNoise sess = runRIO $ do 
+  LoadSignals sigs <- io $ decodeFile $ take 6 sess++"/sigs_"++take 6 sess++"_noise"
+  let initialV = L.fromList [-2, 2::Double, -5, -60]
+  io$ print $ initialV
+  io$ print $ tmax/dt
+  iniampar <- sample $ initialAdaMet 100 5e-3 (posteriorNoiseV sigs) initialV
+  froampar <- runAndDiscard 500 (show . ampPar) iniampar $ adaMet False (posteriorNoiseV sigs)
+  vsamples<- runAdaMetRIO 200 True froampar (posteriorNoiseV sigs)
+  let [logtheta, sigma, logobs, _] = L.toList$   runStat meanF vsamples
+  io $ writeFile (take 6 sess++"/noisePars") $ show (logtheta, sigma, logobs)
+  return ()
+
+measAmps sess = runRIO $ do
+  (logtheta, sigma, logobs) <- fmap read $ io $ readFile (take 6 sess++"/noisePars")
+  let covM = fillM (np,np) $
+              \(i,j)-> ((covOU (exp logtheta) (sigma::Double)) (toD i)) (toD j)+ifObs i j (exp logobs)
+  let invDetails = invlndet covM
+  nms <- fmap read $ io $ readFile (take 6 sess++"/sessions")
+  sigs <- fmap concat $ forM nms $ \sessNm-> do 
+            LoadSignals sigs <- io $ decodeFile $ take 6 sess++"/sigs_"++take 6 sessNm++"_epsps" 
+            return sigs
+  let wf = baselineSig 0.003 $ averageSigs $ sigs
+  h<- io $ openFile (take 6 sess++"/epsps") WriteMode 
+  forM_ sigs $ \sig@(Signal dt t0 _) -> do
+      let initialV = L.fromList [-60,1]
+      iniampar <- sample $ initialAdaMet 100 5e-3 (posteriorSigV wf invDetails sig) initialV
+      froampar <- runAndDiscard 2000 (show . ampPar) iniampar $ adaMet False (posteriorSigV wf invDetails sig)
+      vsamples<- runAdaMetRIO 2000 True froampar (posteriorSigV wf invDetails sig)
+      let (amp,sd) = both meanF stdDevF `runStat` map (@>1) vsamples
+      io $ print (t0,amp,sd)
+      io $ hPutStrLn h $ show (t0, amp,sd)
+      --plotPts $ zip [0..] $ map (@>1) vsamples
+      return ()
+  io $ hClose h
+  return ()
+ 
+measNPQ sess = runRIO $ do
+  let ffile = (unzip3 .  sortBy (comparing fst3) . map read . lines)
+  (t0s'::[Double], amps,sds) <- io $ fmap ffile  $ readFile (take 6 sess++"/epsps")
+  let tsamps = zip t0s' amps
+      --tsamps = filter ((<3) . (\(t, amp)-> zscore tsamps' (t,amp))) tsamps'
+      t0s = map fst tsamps
+  let weighCurve' = map (weighRegression tsamps ) t0s
+      maxPcurve = foldl1 max weighCurve'
+      pcurve = map (/(maxPcurve)) weighCurve'
+  let globalSd = sqrt $ runStat (before meanF (**2)) sds
+
+  let initialV = L.fromList [100,log 0.15,0.8,log 0.02]
+
+  let nsam = 4000
+      nfrozen = 2000 
+  io $ print $ posteriorNPQV amps pcurve globalSd initialV
+  iniampar <- sample $ initialAdaMet 500 5e-3 (posteriorNPQV amps pcurve globalSd) initialV
+  io $ putStr "inipar ="
+  io $ print $ ampPar iniampar
+  froampar <- runAndDiscard nsam (showNPQV . ampPar) iniampar $ adaMet False (posteriorNPQV amps pcurve globalSd)
+  io $ putStr "frozenpar ="
+  io $ print $ ampPar froampar
+  vsamples <- runAdaMetRIO nfrozen True froampar (posteriorNPQV amps pcurve globalSd) 
+  io $ writeFile (take 6 sess++"/npq_samples") $ show vsamples
+  let (mean,sd) =  (both meanF stdDevF) `runStat` vsamples 
+  io $ putStrLn $ intercalate "\t" $ map (minWidth 8) $ words "n cv phi q"
+  io $ putStrLn $ showNPQV mean
+  io $ putStrLn $ showNPQV sd 
+  return ()
+
+fst3 (x,_,_) = x
+
+whenContinues sess mma = do
+      conts <- durations "continues" "foo"
+      sessid <- getSessionName
+      case conts of
+        [] -> if (sessid==sess) then mma else return Nothing
+        (_,s):_ | s `isPrefixOf` sess -> mma
+                | otherwise -> return Nothing
+  
---
+++ FETi/QuantalHelp.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables, NoMonomorphismRestriction, ViewPatterns #-}
+module QuantalHelp where
+import Math.Probably.MCMC
+import Math.Probably.StochFun
+import Math.Probably.Sampler
+import Math.Probably.FoldingStats
+import Control.Monad
+import Data.Array
+import qualified Statistics.Math as SM
+import qualified Math.Probably.Student as S
+import qualified Data.StorableVector as SV
+import Codec.Image.DevIL
+import qualified Data.Array.Unboxed as UA
+import Foreign.ForeignPtr
+import Foreign.Storable.Tuple
+import qualified Data.StorableVector as SV
+import System.IO.Unsafe
+import qualified Math.Probably.PDF as PDF
+import qualified Numeric.LinearAlgebra as L
+import Baysig.Estimate.RTS
+import Math.Probably.RandIO
+import Data.List
+
+neg = \x-> 0.000-x
+square = \x-> x*x
+step = \x-> if (x<0.000) then 0.000 else 1.000
+fac = \_arg0-> case _arg0 of 1 -> 1; n -> n*(fac (n-1))
+assert_fac3 = (fac 3)==6
+assert_pairs0 = 2==(fst ((2,3)))
+assert_pairs1 = 3==(fst ((2,3)))
+assert_case = case 1 of 0 -> False; 1 -> True
+oneOf = \xs-> ((fmap floor)$(uniform 0.000 (realToFrac$(length xs))))>>=(\idx-> return$(ix idx xs))
+uniformLogPdf = \lo-> \hi-> \x-> 
+   if ((x<hi)&&(x>lo)) 
+       then 1.000 
+       else {-trace ("expected "++show x++"to be in rng "++show (lo,hi)) -}(0.000-1.000e30)
+uniform' = \lo-> \hi-> unit>>=(\x-> return ((x*(hi-lo))+lo))
+unormal' = unit>>=(\u1-> unit>>=(\u2-> return ((sqrt ((0.000-2.000)*(log u1)))*(cos ((2.000*pi)*u2)))))
+oneTo = \n-> (uniform 0.500 ((realToFrac n)+0.500))>>=(\x-> return (round x))
+oneToLogPdf = \hi-> \x-> if ((x<(hi+1))&&(x>0)) then 1.000 else (0.000-1.000e20)
+normal = \mean-> \tau-> unormal>>=(\u-> return ((u/(sqrt (tau*2.000)))+mean))
+normalSd = \mean-> \sd-> unormal>>=(\u-> return ((u*sd)+mean))
+normalPdf = \mu-> \tau-> \x-> (sqrt ((tau/2.000)*pi))*(exp (0.000-(((x-mu)*(x-mu))*tau)))
+normalLogPdf :: Double -> Double -> Double -> Double
+normalLogPdf = \mu-> \tau-> \x-> (log (sqrt ((tau/2.000)*pi)))+(0.000-(((x-mu)*(x-mu))*tau))
+sdToTau = \sd-> 1.000/((2.000*sd)*sd)
+meanCVToTau = \mn-> \cv-> 1.000/((2.000*(cv*mn))*(cv*mn))
+varToTau = \var-> 1.000/(2.000*var)
+tauToSD = \t-> 1.000/(sqrt (t*2.000))
+logNormal = \mu-> \tau-> (fmap exp)$(normal mu tau)
+logNormalPdf = \mu-> \tau-> \x-> ((sqrt tau)*(1.000/x))*(exp (0.000-(((tau*((log x)-mu))*((log x)-mu))/2.000)))
+binNormalApprox = \n-> \p-> normal (n*p) (varToTau ((n*p)*(1.000-p)))
+binomialProb = \n-> \p-> \k-> ((choose n k)*(p^k))*((1.000-p)^(n-k))
+binomialLogProb :: Int ->Double ->Int -> Double 
+binomialLogProb = \n-> \p-> \k-> ((log$(choose n k))+((realToFrac k)*(log p)))+((realToFrac (n-k))*(log (1.000-p)))
+countTrue = \_arg0-> case _arg0 of Nil  -> 0; Cons True  bs -> 1+(countTrue bs); Cons False  bs -> countTrue bs
+betaLogPdf = \a-> \b-> \x-> log$(((1.000/(S.beta (realToFrac a) (realToFrac b)))*(x^(a-1)))*((1.000-x)^(b-1)))
+unfoldN = \n-> \m-> \lastx-> \s-> if (n<m) then ((s n lastx)>>=(\x-> (((unfoldN (n+1) m) x) s)>>=(\xs-> return (Cons x xs)))) else ((s n lastx)>>=(\v-> return (Cons v Nil)))
+unfold = \n-> \lastx-> \s-> ((unfoldN 1 n) lastx) s
+binGauss = \ns-> \p-> \q-> \cv-> \bgSd-> (binomial ns p)>>=(\nr-> normal ((realToFrac nr)*q) (varToTau$(((((q*cv)*q)*cv)*(realToFrac nr))+(bgSd*bgSd))))
+binGaussPdf = \ns-> \p-> \q-> \cv-> \bgSd-> \v-> (bigSum 0 ns)$(\nr-> ((normalPdf ((realToFrac nr)*q) (varToTau$(((((q*cv)*q)*cv)*(realToFrac nr))+(bgSd*bgSd)))) v)*((binomialProb ns p) nr))
+binGaussLogPdf = \ns-> \p-> \q-> \cv-> \bgSd-> \v-> log$((bigSum 0 ns)$(\nr-> exp$(((normalLogPdf ((realToFrac nr)*q) (varToTau$(((((q*cv)*q)*cv)*(realToFrac nr))+(bgSd*bgSd)))) v)+((binomialLogProb ns p) nr))))
+normalInit = \mu-> \(_) -> mu
+uniformInit = \lo-> \hi-> (hi+lo)/2.000
+oneToInit = \n-> div n 2
+binGaussInit = \n-> \p-> \q-> \(_) -> \(_) -> ((realToFrac n)*p)*q
+betaInit = \a-> \b-> a/(a+b)
+alpha = \tc-> \t-> ((((step t)*tc)*tc)*t)*(exp ((0.000-t)*tc))
+qsig = \amp-> \tc-> \t0-> \off-> \t-> off+(amp*(alpha tc (t-t0)))
+covOU = \theta-> \sigma-> \s-> \t-> (((sigma*sigma)*0.500)/theta)*(exp (0.000-(theta*(abs (s-t)))))
+dt = 5.000e-5
+tmax = 5.000e-2
+np = round$(tmax/dt)
+toD = \i-> (realToFrac i)*dt
+
+ifObs = \i-> \j-> \sig-> if (i==j) then sig else 0.000
+
+gpByInvLogPdf = \(_) -> \(_) -> \meansig-> \lndet-> \covinv-> \obssig-> let ((dt,_),obsvec) = observe obssig; meanVec = (fillV np)$(\i-> meansig (toD i)) in ((mvnPdf lndet covinv) meanVec) obsvec
+
+posteriorNoiseV sigs v = let (covM)=fillM (((round (5.000e-2/5.000e-5)),(round (5.000e-2/5.000e-5)))) (\(i,j)-> ((((sigma*sigma)*0.500)/(exp logtheta))*(exp (0.000-((exp logtheta)*(abs (((realToFrac i)*5.000e-5)-((realToFrac j)*5.000e-5)))))))+(if (i==j) then (exp logobs) else 0.000)) in let (inv,lndet)=invlndet covM in uniformLogPdf (0.000-50.000) (100.000) logtheta
+ +uniformLogPdf (0.000) (10.000) sigma
+ +uniformLogPdf (0.000-50.000) (100.000) logobs
+ +uniformLogPdf (0.000-80.000) (0.000-40.000) vmean
+ +(sum $ (flip map) (zip [1..10] sigs) $ \(i, sigv)->gpByInvLogPdf (5.000e-5) (5.000e-2) (\y-> vmean) (lndet) (inv) sigv)
+  where logtheta = v@> 0
+        sigma = v@> 1
+        logobs = v@> 2
+        vmean = v@> 3
+
+(@>) = (L.@>)
+
+ouSynapseLogPdf (covinv, lndet) 
+      = \meansig-> \obssig-> let (_,obsvec) = observe obssig
+                                 (_,meanvec) = observe meansig
+                             in ((mvnPdf lndet covinv) meanvec) obsvec
+
+scaleSig off x (Signal dt t0 vec) = Signal dt t0 (L.mapVector (\v-> v*x+off) vec)
+
+baselineSig :: Double -> Signal Double -> Signal Double
+baselineSig tbase (Signal dt tst vec) = 
+   let ntake = round $ tbase/dt
+       basevec = L.subVector 0 ntake vec
+       xsub = (L.foldVector (+) 0 basevec) / realToFrac ntake      
+   in (Signal dt tst (L.mapVector (subtract xsub) vec))
+
+posteriorSigV wf invDetails sig v 
+  = ouSynapseLogPdf invDetails (scaleSig (v0) amp wf) sig
+ where v0 = v@> 0
+       amp = v@> 1
+
+posteriorNPQV amps pcurve sd v = -- ((n,cv,slope,offset,phi,plo,q,tc,t0), loopvals) = 
+ oneToLogPdf (400) n
+ +uniformLogPdf 0.01 0.5 cv
+ +uniformLogPdf (0) (1) phi
+ +uniformLogPdf (0.000) (2.000e-1) q
+ +(sum $ (flip map) (zip pcurve amps) $ \(pcurveVal, amp)->let p=pcurveVal * phi in binGaussLogPdf (n) (p) (q) (cv) (sd) amp)
+  where n = round $ v @> 0
+        cv = exp $ v @> 1
+        phi = v @> 2
+        q = exp $ v @> 3
+
+cut = 500
+
+
+weigh _ [] = []
+weigh t_hat (pt@(t_data,y):rest) 
+  | abs(t_data-t_hat) > cut = weigh t_hat rest
+  | otherwise = replicate (5 - (floor $ abs(t_data-t_hat)/cut * 5)) pt ++ weigh t_hat rest
+
+
+weighRegression :: [(Double,Double)] -> Double -> Double
+weighRegression pts x = y where
+   myPts = weigh x pts -- filter (\(t,y)-> abs(x-t) < cut) pts
+   (slope,offset) = runStat regressF myPts
+   y = slope*x + offset
+
+zscore :: [(Double,Double)] -> (Double,Double) -> Double
+zscore tsamps (t,amp) = abs z where
+  relevant = filter ((< 200) . abs . (`subtract` t) . fst) tsamps
+  (slope, offset) = runStat regressF $ relevant
+  detrended = map (\(t,amp)-> amp - (slope*t + offset)) relevant
+  (mean, sd) = runStat meanSDF $ detrended
+  z = (amp - (slope*t + offset))/ sd
+ 
+
+showNPQV :: L.Vector Double -> String
+showNPQV (L.toList -> [n, logcv, phi, logq]) 
+  = intercalate "\t" $ map (minWidth 8 . accushow) [n, exp logcv, phi, exp logq]
+
+minWidth n s | len < n = s ++ replicate (n - len) ' '
+             | otherwise = s
+  where len = length s
